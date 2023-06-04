d35cbb27333bcf6a4b7e0322d3e532dbd22d9590
PcAudio <byandersj@gmail.com>
2023-06-04 13:28:40 -0500

    3 Booleans

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 
 
-def print_full_name(name,last_name):
-    full_name = name + ' '+ last_name
+def print_booleans(number):
+    is_single =True
+    print(type(is_single))
 
-    print(full_name)
+    print(not True )
 
-def print_full_name_format(name,last_name):
-
-    template ="hola , mi nombre es {} y mis apellidos son {}".format(name,last_name)
-    print(template)
-    print("#-------")
-    template = f'hola soy {name} usando f form'
-    print(template)
+    print(is_single)
+    print(not is_single)
 
 
 if __name__ == '__main__':
-    print_full_name('nicolas','Molina Monroy')
-    print("---")
-    print_full_name_format('Nicolas', 'Molina Monroy')
+    print_booleans(2)
+
 
